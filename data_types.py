80df0b320056fc52e4cab79bb2bca8509aab8e50
Serhiy Takhtay <80953851+SerhiyTakhtay@users.noreply.github.com>
2021-11-24 01:01:11 +0000

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,23 @@
+#int
+#float
+#bool
+#str
+#list
+#tuple
+#set
+#dict
+#(complex) Classes ~ custom types-we can create
+#(complex) Specialized data Types ~ comes from library
+#None
+print (type(2*2))
+print(type(2/4))
+print(type(2<3))
+print(type('Hello'))
+print(type([1,2,3,4]))
+print(type((1,2,3,4)))
+print(type({1,2,3,4}))
+print(type({'may':10,'june':25}))
+print(2**3) #to the power of 3
+print(5//4) # double slash will devide and only keep the whole number component 
+print(6%4) # modulo, returns the remainder of dividing 
+# Operator precedure 1(),2**,3*/ equal from left to right,4+- equal from left to right

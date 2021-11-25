232a1f49b8451dc3cc77a88d7e7b39e6de5a4851
Serhiy Takhtay <80953851+SerhiyTakhtay@users.noreply.github.com>
2021-11-25 23:36:33 +0000

    Add files via upload

---
+++ conditional_logic.py
@@ -0,0 +1,8 @@
+is_old = True
+is_licenced = True
+if is_old:
+	print('You are old enough to drive')
+elif is_licenced:
+	print('You can drive now')
+else:
+	print('You need to be 18 or over and be licenced to drive')

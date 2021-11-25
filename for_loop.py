e48305c3a38d182a2061b142c2dbd9ff7748fe7a
Serhiy Takhtay <80953851+SerhiyTakhtay@users.noreply.github.com>
2021-11-25 23:50:30 +0000

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,11 @@
+#Allows run the same code over and over
+for item in 'ZERO TO MASTERY':
+	#iterable - capable of returning its members one at the time in a for loop
+	print (item)
+
+for intem in (1,2,3,4):
+	print(item)
+
+for item in (1,2,3,4):
+  for x in ['a','b']:
+  	print(item,x)

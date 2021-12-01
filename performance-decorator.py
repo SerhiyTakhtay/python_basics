f63db679b00d22d7c711799fe44abb40b455face
Serhiy Takhtay <80953851+SerhiyTakhtay@users.noreply.github.com>
2021-12-01 22:52:31 +0000

    Add files via upload

---
+++ performance-decorator.py
@@ -0,0 +1,15 @@
+from time import time
+def performance (fn):
+  def wrapper (*args, **kwargs):
+   t1 = time ()
+   result = fn(*args, **kwargs)
+   t2 = time()
+   print (f'it took {t2-t1}s')
+  return wrapper
+
+@performance
+def long_time():
+  for i in range (100000000):
+    i*5
+
+long_time()
